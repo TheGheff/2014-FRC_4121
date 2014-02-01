06d8851bd9c8f48a255b237551b1ef6760d69275
JrCressman-4121 <sgcressman@neo.rr.com>
2014-02-01 10:33:28 -0500

    Update WinchSubsystem.cpp
    added RetractHold state and a StopEverything state. Modified Release state to make sure winch motor is off and the disengage relay is on with the engage relay off.

--- CommandBasedRobotTemplate/Subsystems/WinchSubsystem.cpp
+++ CommandBasedRobotTemplate/Subsystems/WinchSubsystem.cpp
@@ -2,6 +2,7 @@
 #include "../Robotmap.h"
 
 WinchSubsystem::WinchSubsystem() : Subsystem("WinchSubsystem") {
+	//this is the constructor
 	relay1 = new Relay(ENGAGE_WINCH);
 	relay2 = new Relay(DISENGAGE_WINCH);
 	winchMotor = new Jaguar(WINCHMOTOR);
@@ -15,15 +16,30 @@ void WinchSubsystem::InitDefaultCommand() {
 }
 
 void WinchSubsystem::Retract() {
-	relay1->Set(Relay::kOn);
+	relay1->Set(Relay::kOn);//turns on relay1 which initiates pulling back of the launcher
 	relay2->Set(Relay::kOff);
 	winchMotor->SetSpeed(.7);
 }
 
-void WinchSubsystem::Release() {
-	relay1->Set(Relay::kOff); // Turns off some lights in the relay section
+void WinchSubsystem::RetractHold() {
+	//holds the launcher in retract state but turns the winch motor off
+	relay1->Set(Relay::kOn);
+	relay2->Set(Relay::kOff);
+	winchMotor->SetSpeed(0);
 }
 
+
+void WinchSubsystem::Release() {
+	relay1->Set(Relay::kOff); // Turns off some lights in the relay section
+	relay2->Set(Relay::kOn); //turns on relay2 which releases the launcher
+	winchMotor->SetSpeed(0);
+}
+
+void WinchSubsystem::StopEverything(){
+	relay1->Set(Relay::kOff); //stops the pulling back of and the releasing of the winch
+	relay2->Set(Relay::kOff);
+	winchMotor->SetSpeed(0);//winch motor stops running
+}
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
 
