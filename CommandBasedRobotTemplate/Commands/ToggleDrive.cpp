94551fd7e490045f0738f1b0fa1db07372c3573e
TheGheff <armstrong.jeffrey@gmail.com>
2014-02-01 10:16:31 -0500

    Update ToggleDrive.cpp
    Renamed toggle drive command to make the distinction between the command class and the chassis method clearer.

--- CommandBasedRobotTemplate/Commands/ToggleDrive.cpp
+++ CommandBasedRobotTemplate/Commands/ToggleDrive.cpp
@@ -1,30 +1,30 @@
 #include "ToggleDrive.h"
 
-ToggleDrive::ToggleDrive() {
+cmdToggleDrive::cmdToggleDrive() {
 	// Use requires() here to declare subsystem dependencies
 	// eg. requires(chassis);
 	Requires(chassis);
 }
 
 // Called just before this Command runs the first time
-void ToggleDrive::Initialize() {	
+void cmdToggleDrive::Initialize() {	
 }
 
 // Called repeatedly when this Command is scheduled to run
-void ToggleDrive::Execute() {
+void cmdToggleDrive::Execute() {
 	chassis->ToggleDrive();//driveWithJoystick(oi->getDriveStickL(), oi->getDriveStickR());
 }
 
 // Make this return true when this Command no longer needs to run execute()
-bool ToggleDrive::IsFinished() {
+bool cmdToggleDrive::IsFinished() {
 	return false;
 }
 
 // Called once after isFinished returns true
-void ToggleDrive::End() {
+void cmdToggleDrive::End() {
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
-void ToggleDrive::Interrupted() {
+void cmdToggleDrive::Interrupted() {
 }
