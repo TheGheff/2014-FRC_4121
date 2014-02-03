87fdc8d597cb6ccf57dfdad80f29b85c425c1459
JrCressman-4121 <sgcressman@neo.rr.com>
2014-02-03 18:27:15 -0500

    Update LoaderSubsystem.cpp
    added all of the methods from the .h file for this subsystem

--- CommandBasedRobotTemplate/Subsystems/LoaderSubsystem.cpp
+++ CommandBasedRobotTemplate/Subsystems/LoaderSubsystem.cpp
@@ -1,9 +1,10 @@
 #include "LoaderSubsystem.h"
 #include "../Robotmap.h"
 
-LoaderSubsystem::LoaderSubsystem() : Subsystem("LoaderSubsystem") {
-	//this is the constructor
-
+LoaderSubsystem:: LoaderSubsystem () : Subsystem("LoaderSubsystem") 
+{
+	extendSolenoid = new Relay(ENGAGE_SOLENOIDS);
+	retractSolenoid = new Relay(DISENGAGE_SOLENOIDS);
 	feederMotor = new Jaguar(FEEDERMOTOR);
 }
     
@@ -11,16 +12,32 @@ void LoaderSubsystem::InitDefaultCommand() {
 	// Set the default command for a subsystem here.
 	//SetDefaultCommand(new MySpecialCommand());
 }
-
-void LoaderSubsystem::RunLoader() {
-	feederMotor->SetSpeed(.3);
+	
+void LoaderSubsystem::LoaderOn() {
+	feederMotor->SetSpeed(.7);//turns feeder motor on not sure of the speed yet
+	
 }
+void LoaderSubsystem::LoaderOff(() {
+	feederMotor->SetSpeed(0);//stops feeder motor
+	
+}
+void LoaderSubsystem::RelayExtend(){
+	retractSolenoid->Set(Relay::kOff);// make sure solenoids are retracted
+	extendSolenoid->Set(Relay::kOn);//extend the solenoids of the feeder
+	
+
+	
+	
+}
+void LoaderSubsystem::RelayRetract(){
+	extendSolenoid->Set(Relay::kOff);//makes sure solenoids aren’t extended
+	retractSolenoid->Set(Relay::kOn); //retract the solenoids of the feeder
 
 
+	
+}
 void LoaderSubsystem::StopEverything(){
-	feederMotor->SetSpeed(0);//winch motor stops running
+	extendSolenoid ->Set(Relay::kOff); //stops extending and retracting of feeder solenoids 
+	retractSolenoid ->Set(Relay::kOff);
+	feederMotor->SetSpeed(0);//feeder motor stops running
 }
-
-// Put methods for controlling this subsystem
-// here. Call these from Commands.
-
